8e3f1c959d40409d232416d8d3d386f0fde7a928
Charghoul <jan.eisenbarth@googlemail.com>
2016-11-20 18:01:14 +0100

    icons-richtig 0.1

--- src/model/GetIconInfo$Visitor.java
+++ src/model/GetIconInfo$Visitor.java
@@ -2,163 +2,34 @@ package model;
 
 import model.visitor.AnythingVisitor;
 import persistence.*;
+import serverConstants.ToStringConstants;
 
-public class GetIconInfo$Visitor extends model.visitor.AnythingDirectVisitor {
+public class GetIconInfo$Visitor extends model.visitor.AnythingStandardVisitor {
 
 	int result = 0;
+
+	@Override
+	protected void standardHandling(Anything anything) throws PersistenceException {
+	//	anything.accept(this);
+		result = 0;
+	}
+
+	@Override
+	public void handleProduktgruppe(Produktgruppe4Public produktgruppe) throws PersistenceException {
+		result = 2;
+	}
+
+	@Override
+	public void handleEinkaufsManager(EinkaufsManager4Public einkaufsmanager) throws PersistenceException {
+		result = 3;
+	}
+
+
+
 
 	public int getIconInfo(Anything anything) throws PersistenceException {
 		anything.accept(this);
 		return result;
 	}
 
-	@Override
-	public void handleServer(Server4Public server) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handlePositionAbstrakt(PositionAbstrakt4Public positionAbstrakt) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleEinkaufsManager(EinkaufsManager4Public einkaufsManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleErrorDisplay(ErrorDisplay4Public errorDisplay) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleServiceAdminZeitManager(ServiceAdminZeitManager4Public serviceAdminZeitManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleBestellung(Bestellung4Public bestellung) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleHerstellerManager(HerstellerManager4Public herstellerManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleLieferart(Lieferart4Public lieferart) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleServiceKundeBestellManager(ServiceKundeBestellManager4Public serviceKundeBestellManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleCommandCoordinator(CommandCoordinator4Public commandCoordinator) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleKomponente(Komponente4Public komponente) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleThreadOfControl(ThreadOfControl4Public threadOfControl) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleProduktKatalog(ProduktKatalog4Public produktKatalog) throws PersistenceException {
-		result = 2;
-	}
-
-	@Override
-	public void handleBestellstatus(Bestellstatus4Public bestellstatus) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleServiceShopProduktKatalog(ServiceShopProduktKatalog4Public serviceShopProduktKatalog) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleZeitObjekt(ZeitObjekt4Public zeitObjekt) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleArtikelManager(ArtikelManager4Public artikelManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleService(Service4Public service) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleBestellManager(BestellManager4Public bestellManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleZeitManager(ZeitManager4Public zeitManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleSubj(Subj4Public subj) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleKonto(Konto4Public konto) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleHersteller(Hersteller4Public hersteller) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleKunde(Kunde4Public kunde) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleLieferartManager(LieferartManager4Public lieferartManager) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleIntegerWrapper(IntegerWrapper4Public integerWrapper) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleWarenlager(Warenlager4Public warenlager) throws PersistenceException {
-		result = 1;
-	}
-
-	@Override
-	public void handleCommonDate(CommonDate4Public commonDate) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleCommandExecuter(CommandExecuter4Public commandExecuter) throws PersistenceException {
-
-	}
-
-	@Override
-	public void handleArtikelstatus(Artikelstatus4Public artikelstatus) throws PersistenceException {
-
-	}
 }
