389e6dfb47c8d28bb0f3abe2c2a88677e9769b5f
Charghoul <jan.eisenbarth@googlemail.com>
2016-11-20 19:15:27 +0100

    icons-richtig 0.2

--- src/viewClient/IconManager.java
+++ src/viewClient/IconManager.java
@@ -8,13 +8,16 @@ public class IconManager {
 
 	public static Image getImage(int iconInfo) {
 		switch (iconInfo) {
-			case 1: return new Image("/viewResources/Icons/standard.png");
+			case 1: return new Image("/viewResources/Icons/file.png");
 			case 2: return new Image("/viewResources/Icons/folder.png");
-			case 3: return new Image("/viewResources/Icons/einkaufswagen.png");
-
+			case 3: return new Image("/viewResources/Icons/bag_color.png");
+			case 4: return new Image("/viewResources/Icons/Bestellung_transit.png");
+			case 5: return new Image("/viewResources/Icons/Bestellung_finished.png");
+			case 6: return new Image("/viewResources/Icons/shop_color.png");
+			case 7: return new Image("/viewResources/Icons/hersteller.png");
 		// TODO provide images for icon info numbers!
 			default:
-			return new Image("/viewResources/Icons/standard.png");
+			return new Image("/viewResources/Icons/database.png");
 		}
 
 	}
