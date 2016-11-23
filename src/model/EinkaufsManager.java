574bee0441a21d32cbd8c0ca46213f79b726cf2b
Charghoul <jan.eisenbarth@googlemail.com>
2016-11-23 22:35:33 +0100

    Konto: reserviert implementiert und gepflegt, KndAnnahme und KndLieferung funktioniert richtig, Preis der Lieferung auf Bestellung raufgerechnet

--- src/model/EinkaufsManager.java
+++ src/model/EinkaufsManager.java
@@ -293,8 +293,6 @@ public class EinkaufsManager extends PersistentObject implements PersistentEinka
     
     public void bestellen(final Lieferart4Public lieferart) 
 				throws model.ExcWarenwertUeberKontoguthaben, model.ExcArtikelNichtVerfuegbar, PersistenceException{
-        //TODO: überprüfen ob warenmenge+bisherige nicht bezahlte bestellungen  kontoguthaben übersteigt
-        //TODO: betrag der bestellungen und vorbestellungen im konto mit hinterlegen - damit einfacher überprüft werden kann
         long bestellungswert = getThis().gibGesamtPreis();
         if(bestellungswert + getThis().getBestellManager().getKonto().getReserviert() > getThis().getMyServiceKunde().getKonto().getKontostand()) {
             throw new ExcWarenwertUeberKontoguthaben(ErrorMessages.WarenwertUeberKontoguthaben);
